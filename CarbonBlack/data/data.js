de255de989fe053d1359e0f2c0d7e4e455ec8f33
Nandan Jain <jain.nandan@gmail.com>
2015-07-14 19:30:53 -0400

    Added db backup documents from tiers db

---
+++ CarbonBlack/data/data.js
@@ -0,0 +1,50 @@
+/**
+ * Created by Nandy on 7/11/15.
+ */
+var x = {
+    "_id": ObjectId("53cf6ecfb23207855ea7d7fc"),
+    "state": "cancel",
+    "date": "2014-07-23T08:36:37.165Z",
+    "tires": [
+
+    ],
+    "cash": NumberLong(0),
+    "services": [
+        {
+            "name": "Mounting",
+            "price": NumberLong(8),
+            "quantity": NumberLong(1),
+            "checked": true
+        },
+        {
+            "name": "Balancing",
+            "price": NumberLong(10),
+            "quantity": NumberLong(1),
+            "checked": false
+        }, {
+            "name": "Repair",
+            "price": NumberLong(10),
+            "quantity": NumberLong(1),
+            "checked": true
+        }, {
+            "name": "Rotation",
+            "price": NumberLong(10),
+            "quantity": NumberLong(1),
+            "checked": false
+        }
+    ],
+    "discount": {
+        "amount": NumberLong(0),
+        "checked": false
+    }
+    ,
+    "tax": {
+        "rate": NumberLong(6),
+        "amount": 1.08,
+        "checked": true
+    }
+    ,
+    "total": 19.08,
+    "shop_id": 7
+};
+

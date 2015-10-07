4ceb534adc9e0e7da8103d9f434bef34a5a27e08
root <root@instance-1.c.clear-style-109118.internal>
2015-10-07 23:04:24 +0000

    update

--- server.php
+++ server.php
@@ -27,6 +27,17 @@ if (isset($_REQUEST['imageData'])) {
     $file = fopen($filePath, 'w');
     fwrite($file, $data);
     fclose($file);
+
+    $output = shell_exec('br -algorithm FaceRecognition -compare uploads/myImage.png uploads/my.png > uploads/temp');
+    $output1 = shell_exec('cat uploads/temp');
+    error_log("Output of Face Recog.\n" . $output);
+    error_log("Final Score: " . $output1);
+    settype($output1, "integer");
+    if($output1 >= 3) {
+	error_log("The match is SUCCESS !!!!");
+    } else {
+        error_log("The match is FAILED !!!!");
+    }
 } else {
     error_log("Sorry, the key does not exist");
 }
